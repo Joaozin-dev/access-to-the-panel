88af0f8c45e7691080e0cb66a6ab1e835cf8f1ba
Joaozin-dev <josealessandrogg@gmail.com>
2026-02-24 15:06:24 -0300

    Update Admin Access4.lua

--- Admin Access4.lua
+++ Admin Access4.lua
@@ -5,7 +5,6 @@ return {
 
     Admins = {
         omelhorbrookhaven = "2026-01-31",
-        kaue15522 = "permanente",
         JAGAMES94 = "2026-05-01",
         bem_feitoLOL = "2026-05-01"
     }
